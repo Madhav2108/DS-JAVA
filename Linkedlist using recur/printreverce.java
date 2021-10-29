30a040479fa2bac9796270d0d2458a343d1103c8
Madhav Narayan <madhav1471.cse18@chitkara.edu.in>
2021-10-30 04:20:12 +0530

    Add files via upload

---
+++ Linkedlist using recur/printreverce.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Main
+{
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node takeinput()
+    {
+        Scanner s= new Scanner(System.in);
+        int data=s.nextInt();
+        Node head=null;
+        Node tail=null;
+        while(data!=-1)
+        {
+            Node currentNode=new Node(data);
+            if(head==null)
+            {
+                head=currentNode;
+                tail=currentNode;
+            }
+            else{
+                tail.next=currentNode;
+                tail=tail.next;
+            }
+            
+            data=s.nextInt();
+        }
+        return head;
+    }
+    public static void printnode(Node head)
+    {
+        if(head==null)
+        {
+            return;
+        }
+        printnode(head.next);
+        System.out.print(head.data+" ");
+        
+    }
+	public static void main(String[] args) {
+		Node head=takeinput();
+		printnode(head);
+	}
+}
