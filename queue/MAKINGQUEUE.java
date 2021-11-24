0cc34e42b7c908e4889223add070e10b5da2cf0a
Madhav Narayan <madhav1471.cse18@chitkara.edu.in>
2021-11-25 04:26:27 +0530

    Add files via upload

---
+++ queue/MAKINGQUEUE.java
@@ -0,0 +1,78 @@
+import java.util.*;
+public class Main
+{
+    public static class Queue{
+        private int data[];
+        private int front;
+        private int rear;
+        private int size;
+        
+        public static Queue()
+        {
+            data=new int[5];
+            front=-1;
+            rear=-1;
+            
+        }
+        public static Queue(int capacity)
+        {
+            data=new int[capacity];
+            front=-1;
+            rear=-1;
+            
+        }
+        public static int size()
+        {
+            return size;
+        }
+        public static boolean isEmpty(){
+            return size==0;
+        }
+        public static void enqueue(int ele)
+        {
+            if(rear+1==capacity)
+            {
+                System.out.print("queue is full");
+                return-1;
+            }
+            if(size==0)
+            {
+                front=0;
+                rear=0;
+                data[front]=ele;
+            }
+            else
+            {
+                rear++;
+                data[rear]=ele;
+            }
+            size++;
+        }
+        public static int front()
+        {
+            if(size==0)
+            {
+                System.out.print("queue is empty");
+                return-1;
+            }
+            return data[frount];
+        }
+        public static int dequeue()
+        {
+            size--;
+            if(size==0)
+            {
+                int temp=data[front];
+                front=-1;
+                rear=-1;
+                return temp;
+            }
+            front++;
+            
+            return(data[front-1]);
+        }
+    }
+	public static void main(String[] args) {
+		
+	}
+}
