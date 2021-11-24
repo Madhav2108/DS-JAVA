d6976135841b2ace2bc4ca7439dc79e252dfd281
Madhav Narayan <madhav1471.cse18@chitkara.edu.in>
2021-11-25 04:29:37 +0530

    Add files via upload

---
+++ Trees/numnodes.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class Main
+{
+    public static class treenode
+    {
+        public int data;
+        public ArrayList<treenode> children;
+        public treenode(int data){
+            this.data=data;
+            children=new ArrayList<>();
+            
+        }
+    }
+    public static treenode input(Scanner s)
+    {
+        int data=s.nextInt();
+        treenode root=new treenode(data);
+        int childcount=s.nextInt();
+        for(int i=0;i<childcount;i++)
+        {
+            treenode child=input(s);
+            root.children.add(child);
+        }
+        return root;
+        
+    }
+    public static void printtree(treenode root)
+    {
+        String s=root.data+":";
+        for(int i=0;i<root.children.size();i++)
+        {
+            s=s+root.children.get(i).data+",";
+        }
+        System.out.println(s);
+        for(int i=0;i<root.children.size();i++)
+        {
+            printtree(root.children.get(i));
+        }
+    }
+    public static treenode inputwise()
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Root Data");
+        int rootdata=s.nextInt();
+        Queue<treenode> pendindnodes=new LinkedList<treenode>();
+        treenode root= new treenode(rootdata);
+        pendindnodes.add(root);
+        while(!pendindnodes.isEmpty())
+        {
+            treenode frontnode=pendindnodes.poll();
+            System.out.println("Enter num of children of "+ frontnode.data);
+            int numchild=s.nextInt();
+            for(int i=0;i<numchild;i++)
+            {
+                System.out.println("Enter"+(i+1)+"th child of "+ frontnode.data);
+                int child=s.nextInt();
+                treenode childnode= new treenode(child);
+                frontnode.children.add(childnode);
+                pendindnodes.add(childnode);
+            }
+        }
+        return root;
+        
+    }
+    public static int numnodes(treenode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int count=1;
+        for(int i=0;i<root.children.size(); i++)
+        {
+            count+=numnodes(root.children.get(i));
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+// 		Scanner s=new Scanner(System.in);
+// 		treenode root=input(s);
+
+        treenode root=inputwise();
+		printtree(root);
+		System.out.print("Total numbers of nodes->"+ numnodes(root));
+		
+		
+	}
+}
